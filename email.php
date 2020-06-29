f4ea5948e9d7716a10ab5c3ef38ee5508ab5fdaa
Jackbotein <51533320+Jackbotein@users.noreply.github.com>
2020-06-29 13:01:22 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,5 +1,5 @@
 <?php
-    mail('youremail@example.com', $_POST['subject'], $_POST['message']);
+    mail('Jackbotein@gmail.com', $_POST['subject'], $_POST['message']);
 ?>
 <p>Your email has been sent.</p>
 ?>
