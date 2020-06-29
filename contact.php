c439115a453513354cc81920aeae39d7321f27d3
Jackbotein <51533320+Jackbotein@users.noreply.github.com>
2020-06-29 10:00:21 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,15 @@
+/*<?php
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "lipiec002@wp.pl";
+  $headers = "Od: ".$email;
+  $txt = "Otrzymałeś maila od ".$name.". \n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
+*/
